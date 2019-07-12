dd8d75aa581ab8440f1e0b252672ab6697f83000
tobie.tourre <tobie.tourre@ipilyon.net>
2019-07-12 14:15:21 +0200

    templates + fixtures

---
+++ src/DataFixtures/BlogPostFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BlogPost;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class BlogPostFixtures
+ * @package App\DataFixtures
+ * creates fake data
+ */
+class BlogPostFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        for($i = 0; $i < 20 ; $i++){
+            $blogPost = new BlogPost();
+            $blogPost->setTitle('title' . $i);
+            $blogPost->setSlug('slug ' . $i);
+            $blogPost->setDate('200' . $i);
+            $blogPost->setCategory('category' . $i);
+            $blogPost->setFeatured($i%2 === 0 ? true : false);
+            $manager->persist($blogPost);
+        }
+        $manager->flush();
+    }
+}
